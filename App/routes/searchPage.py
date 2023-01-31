103f6df2953b61258be01fe57f42447ddb072030
oellis20 <oellis1@my.waketech.edu>
2023-01-31 16:17:10 -0500

    Add submission functionality to search.html

--- App/routes/searchPage.py
+++ App/routes/searchPage.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('searchPage', __name__)
 
 @bp.route('/', methods=['GET', 'POST'])
 def searchPage():
     return render_template("search.html", title="Search")
+
+@bp.route('/results')
+def searchResults():
+    return render_template("search_results.html", title="Results")
