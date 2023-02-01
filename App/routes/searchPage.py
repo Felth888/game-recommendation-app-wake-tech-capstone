6473ff50c3c4ebc58d6784dafe554483619eaeff
oellis20 <oellis1@my.waketech.edu>
2023-01-31 20:33:10 -0500

    Attempt tp attach search page to search results

--- App/routes/searchPage.py
+++ App/routes/searchPage.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 
 bp = Blueprint('searchPage', __name__)
 
 @bp.route('/', methods=['GET', 'POST'])
 def searchPage():
+    if request.method == "POST":
+        name = request.form['name']
+        print(name)
+        return(redirect(url_for('searchResults')))
+        
     return render_template("search.html", title="Search")
 
-@bp.route('/results')
+@bp.route('/results', methods=["GET", "POST"])
 def searchResults():
-    return render_template("search_results.html", title="Results")
+    # print the page with the query listed
+    return render_template("search-results.html")
