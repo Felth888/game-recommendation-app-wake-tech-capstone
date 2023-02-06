9e4431fcea703bb7c4cb5cfea81d439c7caf9e4e
ldbennett3 <doommarine93@gmail.com>
2023-02-06 16:06:57 -0500

    Fixed CORS error on API key

--- App/routes/searchPage.py
+++ App/routes/searchPage.py
@@ -6,16 +6,11 @@ bp = Blueprint('searchPage', __name__)
 @bp.route('/', methods=['GET', 'POST'])
 def searchPage():
     if request.method == "POST":
-        nameInput = request.form['nameInput'] or ""
-        genreInput = request.form['genreInput'] or ""
-        return(redirect(url_for('/results', nameInput=nameInput, genreInput=genreInput)))
+        return(redirect(url_for('/results')))
         
     return render_template("search.html", title="Search", api_key = current_app.config['IGDB_API_KEY'])
 
 @bp.route('/results', methods=["GET", "POST"])
 def searchResults():
     # print the page with the query listed
-    if request.method == "POST":
-        nameInput = request.args.get('nameInput') or "Test"
-        genreInput = request.args.get('genreInput') or "Testset" 
     return render_template("search-results.html", nameInput=nameInput, genreInput=genreInput) 
