ba55314fc282e9cf3d0597a139875b1e69adec1c
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2023-02-15 10:21:27 -0500

    added user data to profile route

--- App/routes/userProfile.py
+++ App/routes/userProfile.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
 bp = Blueprint('profile', __name__)
 
-@bp.route('/profile', methods=['GET', 'POST'])
+@bp.route('/profile/<user_name>', methods=['GET', 'POST'])
 @login_required
-def profile():
-    return render_template("new-account.html")
+def profile(username):
+    user = User.query.filter_by(user_name=user_name).first_or_404()
+    return render_template("new-account.html", user=user)
 
