0b4d4d29e731c2ae04b7d338194c7b0a70f5472d
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2023-02-19 13:06:22 -0500

    added userProfile to routes, changed to call user-profile.html, added the passing of username to the profile url to base.html

--- App/routes/__init__.py
+++ App/routes/__init__.py
@@ -2,9 +2,10 @@ from flask import Blueprint
 
 
 def init_app(app):
-    from . import loginPage, newAccount, searchPage
+    from . import loginPage, newAccount, searchPage, userProfile
 
     app.register_blueprint(loginPage.bp)
     app.register_blueprint(searchPage.bp)
     app.register_blueprint(newAccount.bp)
+    app.register_blueprint(userProfile.bp)
     
--- App/routes/userProfile.py
+++ App/routes/userProfile.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
 from flask_login import login_user, logout_user, current_user, login_required
-bp = Blueprint('profile', __name__)
+bp = Blueprint('userProfile', __name__)
+from ..models.userModels import User
 
 @bp.route('/profile/<user_name>', methods=['GET', 'POST'])
 @login_required
-def profile(username):
+def profile(user_name):
     user = User.query.filter_by(user_name=user_name).first_or_404()
-    return render_template("new-account.html", user=user)
+    return render_template("user-profile.html", user=user)
 
