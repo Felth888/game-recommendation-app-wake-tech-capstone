ba87d1418b9e9ff389653bbe0f16a7c190a73d1e
ldbennett3 <doommarine93@gmail.com>
2023-02-25 01:49:24 -0500

    Added jQuery & AJAX functionality. Added routing for library/wishlist buttons

--- App/routes/searchPage.py
+++ App/routes/searchPage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from flask import Blueprint, render_template, request, redirect, url_for, current_app, json
 bp = Blueprint('searchPage', __name__)
 from ..models.userModels import User
 
@@ -6,3 +6,19 @@ from ..models.userModels import User
 def searchPage():
     return render_template("search.html", title="Search", api_key = current_app.config['IGDB_API_KEY'])
     
+@bp.route('/add_to_library', methods=['POST'])
+def add_to_library():
+    # A JSON array isnt really needed to return, a string could be used
+    # But a JSON array gives better control for multiple pieces of data
+    returnArr = {'id':request.form['id']}
+    return returnArr
+
+@bp.route('/add_to_wishlist', methods=['POST'])
+def add_to_wishlist():
+    returnArr = {'id':request.form['id']}
+    return returnArr
+
+@bp.route('/update_library', methods=['POST'])
+def update_library():
+    returnArr = {'id':request.form['id']}
+    return returnArr
