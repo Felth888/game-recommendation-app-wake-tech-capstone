1d7d9a1f83a378bd5b4d5983b9f83eed86e2b09b
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2023-03-01 16:10:09 -0500

    added gamelibrary bp to init

--- App/routes/__init__.py
+++ App/routes/__init__.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 
 
 def init_app(app):
-    from . import loginPage, newAccount, searchPage, userProfile, recommendationPage
+    from . import loginPage, newAccount, searchPage, userProfile, recommendationPage, gameLibrary
 
     app.register_blueprint(loginPage.bp)
     app.register_blueprint(searchPage.bp)
     app.register_blueprint(newAccount.bp)
     app.register_blueprint(userProfile.bp)
     app.register_blueprint(recommendationPage.bp)
-    
+    app.register_blueprint(gameLibrary.bp)
