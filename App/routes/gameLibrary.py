cbfe8c2a51542f4e82b4833674eafd47ef0aa0ff
oellis20 <oellis1@my.waketech.edu>
2023-03-04 18:43:25 -0500

    Change game library list
    --still broken

--- App/routes/gameLibrary.py
+++ App/routes/gameLibrary.py
@@ -9,11 +9,8 @@ bp = Blueprint('gameLibrary', __name__)
 @bp.route('/gamelibrary', methods=['GET', 'POST'])
 @login_required
 def game_library():
-    library = UserGame.query.filter_by(user_id=current_user.id).first()
-    game_id = Game.query.filter_by(id=UserGame.game_id)
-    game_list = []
-
-    for game in game_id:
-        game_list.append(game)
+    library = UserGame.query.filter_by(user_id=current_user.id).all() # Get all the user's games via their id
+    game_ids = [g.game_id for g in library] # Store all the ID's for the user's games in a list variable
+    game_list = Game.query.filter_by(Game.id.in_(game_ids)).all() # Get all games with an ID in the list 
         
     return render_template("game-library.html", library=library, game_list=game_list)
