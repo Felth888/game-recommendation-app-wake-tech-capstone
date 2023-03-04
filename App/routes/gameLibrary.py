af2d81f3b30892bd8769cbee6b9caa1933f8190f
oellis20 <oellis1@my.waketech.edu>
2023-03-03 19:38:17 -0500

    Fix missing return

--- App/routes/gameLibrary.py
+++ App/routes/gameLibrary.py
@@ -16,4 +16,4 @@ def game_library():
     for game in game_id:
         game_list.append(game)
         
-    render_template("game-library.html", library=library, game_list=game_list)
+    return render_template("game-library.html", library=library, game_list=game_list)
