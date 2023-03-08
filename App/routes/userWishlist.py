9abce326836ea4468d3761a3af72318f3f9843d5
ldbennett3 <doommarine93@gmail.com>
2023-03-08 17:34:54 -0500

    Wishlist optimization

--- App/routes/userWishlist.py
+++ App/routes/userWishlist.py
@@ -9,13 +9,8 @@ bp = Blueprint('userWishlist', __name__)
 @bp.route('/wishlist', methods=['GET', 'POST'])
 @login_required
 def wishlist():
-    library = UserGame.query.filter_by(user_id=current_user.id).all() # Get all the user's games via their id
-    wishlist_id = []
-    for w in library:
-        if w.wishlist:
-            wishlist_id.append(w.game_id)
+    wishlist = UserGame.query.filter(UserGame.user_id == current_user.id, UserGame.wishlist == True).all()
+    wishlist_id = [w.game_id for w in wishlist]
     w_list = Game.query.filter(Game.id.in_(wishlist_id)).all()
-    #game_ids = [g.game_id for g in library] # Store all the ID's for the user's games in a list variable
-    #game_list = Game.query.filter(Game.id.in_(game_ids)).all() # Get all games with an ID in the list 
         
-    return render_template("wishlist.html", library=library, w_list=w_list)
+    return render_template("wishlist.html", w_list=w_list)
