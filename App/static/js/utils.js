2e6eb395366cfc37b7b1326c21066b7faea18160
ldbennett3 <doommarine93@gmail.com>
2023-04-05 17:40:18 -0400

    Header changes and processing text

--- App/static/js/utils.js
+++ App/static/js/utils.js
@@ -43,6 +43,20 @@ var ID = function(elementID) {
 }
 
 
+
+/**
+ * Creates a promise and sets it on a timeout of the given millisecond value. 
+ * Used with an 'await' to pause asynchronous functions if needed.
+ * Ex: Pausing a function to allow a DOM's display change to appear
+ * @param {int} milliseconds The number of milliseconds to delay
+ * @return {Promise} A promise response set to trigger after provided milliseconds 
+ */
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
 /**
  * Creates a table of arr's data points, using valString to 
  * specify an index for data to fill the cell with. Used to create
